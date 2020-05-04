40875e2158e70649afad5a88c5f1074e22eac0d0
Matheus Cardoso <mtzimba@gmail.com>
2020-05-04 17:59:32 -0300

    Configuração do Firebase, Angular Material e inicio do CRUD dos centros de evangelização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { environment } from '../environments/environment';
+import { EvangelizationCenterModule } from './evangelization-center/evangelization-center.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+ 	  AngularFirestoreModule,
+    BrowserAnimationsModule,
+    EvangelizationCenterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
